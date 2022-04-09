51a64c5e250f264829cd9ab7a7b522c0bb17ad7e
Hongjia Liu <hongjia_liu@outlook.com>
2022-04-09 23:51:01 +0100

    Update Review layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Review from "./Review";
+
 function App() {
-	return <h2>Reviews Project Setup</h2>;
+	return (
+		<main>
+			<section className="container">
+				<div className="title">
+					<h2>Our Reviews</h2>
+					<div className="underline"></div>
+				</div>
+				<Review />
+			</section>
+		</main>
+	);
 }
 
 export default App;
